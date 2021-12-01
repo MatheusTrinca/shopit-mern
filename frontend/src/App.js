0cfadfd80ae649828e7ccc08442491b11d648161
MatheusTrinca <matheusfelipetrinca@gmail.com>
2021-12-01 14:20:30 -0300

    homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
-import Footer from './components/Footer';
-import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container container-fluid">
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
