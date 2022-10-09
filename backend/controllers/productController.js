99b9e73352198965246d4d19ba4a2d4084bf54f1
MatheusTrinca <matheusfelipetrinca@gmail.com>
2022-10-09 20:40:58 -0300

    filtering review

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -18,6 +18,7 @@ exports.getProducts = catchAsyncErrors(async (req, res, next) => {
 
   res.status(200).json({
     success: true,
+    count: products.length,
     productsCount,
     resPerPage,
     products,
